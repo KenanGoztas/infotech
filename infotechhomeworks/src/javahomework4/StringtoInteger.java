4065058ec75b57d1b99f19380a6cad8368d9c1ab
Kenan Göztas <kenangoz0106@gmail.com>
2022-12-18 02:56:41 +0100

    to convert a string to an integer

---
+++ infotechhomeworks/src/javahomework4/StringtoInteger.java
@@ -0,0 +1,27 @@
+package javahomework4;
+
+public class StringtoInteger {
+
+	public static void main(String[] args) {
+		
+		String str = "12345";
+		int i1= Integer.parseInt(str);
+		int i2= Integer.valueOf(str);
+		
+		
+		
+		
+		
+		int i3= 123456;
+		
+		String str2 = String.valueOf(i3);
+		String str3= ""+ i3;
+		String str4= Integer.toString(i3);
+		
+		
+		
+		
+		
+	}
+
+}
